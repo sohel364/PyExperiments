23ce52db36624e34ee8cfc7c52f054e330f6f4a2
sohel364 <sohel.official@gmail.com>
2022-03-22 20:52:31 +0600

    Improvements from ubuntu pc

--- Lec-5/Lec5OOp.py
+++ Lec-5/Lec5OOp.py
@@ -4,14 +4,21 @@ class Employee(object) :
         self.designation = designation
         self.department = department
         self.age = age
-
+        self._salary = 10000
+                
     def DisplayEmployeeInfo(self):
         self.orphanItem = 10
         if self.age > 35 :
             print("Employee details", self.name, self.designation, self.department, self.age)
         else :
             print("There was no one above 35 !")
+    
 
+    def PublicApi():
+        print('I am a public API')
+    
+    def _PrivateApi():
+        print('I am a private API')
 
 e1 = Employee('mhas', 'mis', 'se', 35)
 e1.DisplayEmployeeInfo()
---
+++ Lec-5/UnderScopeInPython.py
@@ -0,0 +1,8 @@
+import Lec5OOp
+from Lec5OOp import *
+
+empObj = Lec5OOp.Employee('sohel', 'cpp', 'se', 32)
+
+print(empObj._salary)
+empObj.PublicApi()
+empObj._PrivateApi()
