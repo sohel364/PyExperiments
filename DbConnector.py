805a58913f897effbec1781b12f78d1665c69c0f
Mahmudul Hasan <sohel.official@gmail.com>
2022-03-25 02:30:49 +0600

    Database connector

---
+++ DbConnector.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+connection = mysql.connector.connect(host='localhost', user='root', password='admin')                            
+mycursor = connection.cursor()
+mycursor.execute("SHOW DATABASES")
+
+for database in mycursor :
+    print(database)
+
+
+
+#Connection by json configuration
+# config = {
+#   'user': 'scott',
+#   'password': 'password',
+#   'host': '127.0.0.1',
+#   'database': 'employees',
+#   'raise_on_warnings': True
+# }
+
+# cnx = mysql.connector.connect(**config)
