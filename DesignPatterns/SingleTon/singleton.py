0a1a5c29d8722dae893af0817a91b578b18d7744
Mahmudul Hasan <sohel.official@gmail.com>
2022-04-05 00:52:40 +0600

    Design Single Ton related topics

--- DesignPatterns/SingleTon/singleton.py
+++ DesignPatterns/SingleTon/singleton.py
@@ -1,1 +1,13 @@
-class_name = 'singleton'
+class SingleTonClass(object):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(SingleTonClass, cls).__new__(cls)
+        return cls.instance
+
+singleton = SingleTonClass()
+new_singleton = SingleTonClass()
+
+print(singleton is new_singleton)
+
+singleton.singl_variable = "Sample variable"
+print(new_singleton.singl_variable)
